0bfd36166ac59a23cadb250bbe65e831035efcb2
allpaul0 <paulallaireperso@gmail.com>
2024-10-24 16:55:58 +0200

    script start_execution_infos for every TPG folder

---
+++ scripts/start_execution_infos.rb
@@ -0,0 +1,167 @@
+#!/usr/bin/ruby
+
+require 'fileutils'
+
+### Script description ###
+
+# For a given TPG (TPG/XXX in the hierarchy), using the SEED passed as input param,
+# launch the program mainExecutionInfos 
+# See the corresponding README.md in TPG
+
+def check_exit_status(code)
+    if code != 0
+      puts "\033[1;91mERROR\033[0m --- Something wrong happened, last exit code is #{code}, exiting script"
+      exit 1
+    end
+end
+
+def training_done?(d)
+    training_dir = File.join(d, 'training')
+    if File.directory?(training_dir)
+      true
+    else
+      puts "\033[1;36mSkip #{d} \033[1;31m(training dir is missing)\033[0m\n"
+      false
+    end
+end
+  
+def codegen_done?(d)
+    codegen_dir = File.join(d, 'CodeGen')
+    if File.directory?(codegen_dir)
+      true
+    else
+      puts "\033[1;36mSkip #{d} \033[0m(CodeGen is missing)\n"
+      false
+    end
+end
+  
+
+### Script ###
+
+# Check if script is called properly
+if ARGV.length < 3
+    puts "Usage: ruby script_name.rb <verbose: true/false> <seed> <name of TPG dir (default: \"TPG\")>"
+    exit 1
+end
+  
+# Convert the string input to boolean value for verbose
+verbose = ARGV[0].casecmp('true').zero?
+
+# Extract seed from command line argument
+seed = ARGV[1].to_i
+
+# Ensure the third argument is a non-empty string
+TPG_dir = ARGV[2]
+if TPG_dir.nil? || TPG_dir.strip.empty?
+    puts "Error: The name of TPG dir must be a non-empty string."
+    puts "Usage: ruby script_name.rb <verbose: true/false> <seed> <name of TPG dir (default: \"TPG\")>"
+    exit 1
+end
+    
+
+# move from the current dir to Trainer-Generator 
+script_dir = File.expand_path(File.dirname(__FILE__))
+Dir.chdir(File.join(script_dir, '..'))
+check_exit_status($?.to_i)
+
+# Trainer-Generator CMake configuration 
+FileUtils.rm_r('Trainer-Generator/bin')
+check_exit_status($?.to_i)
+
+FileUtils.mkdir('Trainer-Generator/bin')
+check_exit_status($?.to_i)
+
+Dir.chdir('Trainer-Generator/bin')
+check_exit_status($?.to_i)
+
+`cmake ..`
+check_exit_status($?.to_i)
+
+
+# launch Cmake command
+trainor_cmake_string = "cmake .."
+unless (verbose)
+  trainor_cmake_string += " > /dev/null 2>&1"
+end
+system(trainor_cmake_string)
+check_exit_status($?.to_i)
+
+
+# go to TPG dir
+Dir.chdir("../../#{TPG_dir}") 
+check_exit_status($?.to_i)
+
+
+# Select directories for which there is a need to do CodeGen
+dirs = Dir.entries('.').reject { |d| d == '.' || d == '..' || d == 'README.md'}
+
+  
+# check if the object is a dir, if training has been done 
+# (required) and if codegen has as well already been done
+dirs = dirs.reject do |d|
+    if File.directory?(d) && training_done?(d) && codegen_done?(d)
+        false
+        #puts "Valid dir #{d}"
+    else
+        true #reject
+    end
+end
+
+
+Dir.chdir('..') 
+check_exit_status($?.to_i)
+
+
+#Do ExecutionInfos
+dirs.each do |d|
+    # Be aware that folder name #{d} can cause encoding problems during the copy operation
+    puts "\033[1;36mExecutionInfos for #{d}\033[0m"
+
+    # required files for inference on x86
+    # system("cp #{TPG_dir}/#{d}/src/instructions.cpp Trainer-Generator/src/")
+    # check_exit_status($?.to_i)
+    # system("cp #{TPG_dir}/#{d}/src/params.json Trainer-Generator/")
+    # check_exit_status($?.to_i)
+  
+    Dir.chdir('Trainer-Generator/bin') do
+
+        trainor_cmake_string = "cmake"
+        
+        # Specify that we are doing Precalcul on int data type
+        # the data needs to be scaled accordingly
+        if d.include?('int')   
+            trainor_cmake_string += " -DTYPE_INT=1 .."
+        else
+            trainor_cmake_string += " -DTYPE_INT=0 .."
+        end
+
+        unless (verbose)
+            trainor_cmake_string += " > /dev/null 2>&1"
+        end
+        
+        system(trainor_cmake_string)
+        check_exit_status($?.to_i)
+
+        trainor_compilation_string = "make ExecutionInfos"
+        unless (verbose)
+            trainor_compilation_string += " > /dev/null 2>&1"
+        end
+
+        system(trainor_compilation_string)
+        check_exit_status($?.to_i)
+
+        Dir.chdir('Release') do
+
+            # Launch the PreCalcul of pairs {seed, nbActionsToTerminal}            
+            system("./ExecutionInfos ../../../#{TPG_dir}/#{d}/CodeGen/best_root_pruned.dot #{seed}")# best root codeGen
+            check_exit_status($?.to_i)
+            
+            # Results are automatically saved to the right folder in ExecutionInfos
+
+            # # Save the results of the ExectionInfos into the concerned TPG dir
+            # system("mv Results ../../../#{TPG_dir}/#{d}/training")
+            # check_exit_status($?.to_i)
+        
+        end
+    end
+end
